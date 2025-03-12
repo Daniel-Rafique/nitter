8c8cf7b1f086e0180f1c515ff40c2ef11150fe1d
Daniel-Rafique <drafique@tfd.ie>
2025-03-12 01:57:15 +0000

    Rolledback

--- src/views/general.nim
+++ src/views/general.nim
@@ -80,11 +80,11 @@ proc renderEnhancedSearch(): VNode =
       text "Search Powered by "
       a(href="https://koyn.ai", target="_blank"): text "Koyn.ai"
       
-    tdiv(class="openbb-badge"):
-      text "Compatible with "
-      a(href="https://openbb.co/", target="_blank"):
-        img(src="/openbb-logo.svg", alt="OpenBB Logo")
-        text "OpenBB Workspace"
+    # tdiv(class="openbb-badge"):
+    #   text "Compatible with "
+    #   a(href="https://openbb.co/", target="_blank"):
+    #     img(src="/openbb-logo.svg", alt="OpenBB Logo")
+    #     text "OpenBB Workspace"
 
 proc renderHead*(prefs: Prefs; cfg: Config; req: Request; titleText=""; desc="";
                  video=""; images: seq[string] = @[]; banner=""; ogTitle="";
